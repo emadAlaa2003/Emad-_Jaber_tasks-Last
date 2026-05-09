f6ec80118a983cef8d07bc6f2ac996b0af50f752
Emad Jaber <emadjbr2003@gmail.com>
2026-05-09 22:29:16 +0300

    Sending data from Frontend to Controller and handling Arrays

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use Faker\Factory;
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\View;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/about', function() {
+    $name='emad';
+        $departments = [
+        '1' => 'Technical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+    ];
+    //return view('about')->with('name', $name);
+    //return view('about', compact('name'));
+    return view('about', compact('name', 'departments'));
+});
+Route::post('/about', function() {
+    $name = $_POST['name'];
+    $departments = [
+        '1' => 'Technical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+    ];
+
+    return view('about', compact('name', 'departments'));
 });
